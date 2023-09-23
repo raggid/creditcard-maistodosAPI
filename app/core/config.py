13d23ba2f2fa79ec653134d99f94f169a6a2f74d
Antonio Raggid <antonioraggid@gmail.com>
2023-09-23 16:05:21 -0400

    adding settings module

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+import secrets
+
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "adminpass"
+    POSTGRES_DB: str = "postgres"
+    SQLALCHEMY_DATABASE_URI: str = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB or ""}'
+
+    EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
+    FIRST_SUPERUSER: EmailStr = "admin@mail.com"
+    FIRST_SUPERUSER_PASSWORD: str = "superpass"
+    USERS_OPEN_REGISTRATION: bool = False
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
