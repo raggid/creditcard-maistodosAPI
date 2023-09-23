56ba32a1cb78a2a9d8ce929b6987d8dccde59903
Antonio Raggid <antonioraggid@gmail.com>
2023-09-23 12:11:08 -0400

    adding response models

--- creditcard/api/v1/creditcard.py
+++ creditcard/api/v1/creditcard.py
@@ -1,25 +1,28 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 import models
-from api import dependencies
-from schemas.creditcard import CreditCardBase
+from api.dependencies import get_database
+from schemas.creditcard import CreditCardBase, CreditCardDB
 
-router = APIRouter(prefix="/credit-card", tags=["credit-cards"])
+router = APIRouter(
+    prefix="/credit-card",
+    tags=["credit-cards"])
 
 
-@router.get("/")
-def get_all_creditcards(db: Session = Depends(dependencies.get_database)):
+@router.get("/", response_model=List[CreditCardDB])
+def get_all_creditcards(db: Session = Depends(get_database)):
     return db.query(models.CreditCard).all()
 
 
-@router.get("/{id}")
-def get_creditcard(id: int, db: Session = Depends(dependencies.get_database)):
+@router.get("/{id}", response_model=CreditCardDB)
+def get_creditcard(id: int, db: Session = Depends(get_database)):
     return db.query(models.CreditCard).filter(models.CreditCard.id == id).first()
 
 
-@router.post("/")
-def create_creditcard(data: CreditCardBase, db: Session = Depends(dependencies.get_database)):
+@router.post("/", response_model=CreditCardDB)
+def create_creditcard(data: CreditCardBase, db: Session = Depends(get_database)):
     creditcard = models.CreditCard(**data.model_dump())
     db.add(creditcard)
     db.commit()
