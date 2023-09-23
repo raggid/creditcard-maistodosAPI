771edb03fe307041b4a6e9d76ad2707b204d0085
Antonio Raggid <antonioraggid@gmail.com>
2023-09-23 12:00:42 -0400

    basic routes

--- creditcard/api/v1/creditcard.py
+++ creditcard/api/v1/creditcard.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 import models
 from api import dependencies
+from schemas.creditcard import CreditCardBase
 
 router = APIRouter(prefix="/credit-card", tags=["credit-cards"])
 
@@ -11,6 +12,16 @@ router = APIRouter(prefix="/credit-card", tags=["credit-cards"])
 def get_all_creditcards(db: Session = Depends(dependencies.get_database)):
     return db.query(models.CreditCard).all()
 
+
 @router.get("/{id}")
 def get_creditcard(id: int, db: Session = Depends(dependencies.get_database)):
     return db.query(models.CreditCard).filter(models.CreditCard.id == id).first()
+
+
+@router.post("/")
+def create_creditcard(data: CreditCardBase, db: Session = Depends(dependencies.get_database)):
+    creditcard = models.CreditCard(**data.model_dump())
+    db.add(creditcard)
+    db.commit()
+    db.refresh(creditcard)
+    return creditcard
