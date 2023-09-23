6fc8a0b08dce14b770d7101bc95c544f523af9af
Antonio Raggid <antonioraggid@gmail.com>
2023-09-23 16:06:25 -0400

    moving database uri to settings module

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql://postgres:adminpass@localhost:5432/postgres", pool_pre_ping=True)
+from core.config import settings
+
+engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
