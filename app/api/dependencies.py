066ae3956340d9a0cbdc56b83f54811aad17568b
Antonio Raggid <antonioraggid@gmail.com>
2023-09-23 16:11:44 -0400

    adding authentication route and token validation

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,4 +1,20 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from pydantic import ValidationError
+from jose import jwt
+
+import models
+import schemas
+from services.user_service import user_service
 from db.session import SessionLocal
+from core.config import settings
+from core import security
+
+
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl="/api/v1/login/access-token"
+)
 
 
 def get_database():
@@ -7,3 +23,22 @@ def get_database():
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_database), token: str = Depends(reusable_oauth2)
+) -> models.User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = schemas.TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = user_service.get_by_id(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-from api.v1.creditcard import router
+from fastapi import APIRouter, Depends
+from api.dependencies import reusable_oauth2
+from api.v1 import creditcard, login
 
 api_router = APIRouter(prefix="/api/v1")
-api_router.include_router(router)
+api_router.include_router(creditcard.router, dependencies=[Depends(reusable_oauth2)])
+api_router.include_router(login.router)
