3cc9cee0b423cdda8d567d00b543f628c96cc6f3
Antonio Raggid <antonioraggid@gmail.com>
2023-09-24 13:16:30 -0400

    moving api prefix

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter, Depends
 from api.dependencies import reusable_oauth2
 from api.v1 import creditcard, login
 
-api_router = APIRouter(prefix="/api/v1")
+api_router = APIRouter()
 api_router.include_router(creditcard.router, dependencies=[Depends(reusable_oauth2)])
 api_router.include_router(login.router)
